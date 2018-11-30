daccf33c7919d7d698c577febb3cb1725ef36bd7
emotley <45399797+emotley@users.noreply.github.com>
2018-11-30 15:45:56 +0000

    Update BruteForceMPI.c

--- BruteForceMPI.c
+++ BruteForceMPI.c
@@ -104,7 +104,8 @@ int main(int argc, char *argv[])
     int posn;
 
     
-    MPI_Init(&argc, &argv);
+    //MPI_Init(&argc, &argv);
+    MPI_Init;
     MPI_Comm_rank (MPI_COMM_WORLD, &id);
     MPI_Comm_size (MPI_COMM_WORLD, &procs);
     
